d306fb9d721491465ad6a1d903fc091f97de8a95
Damien Golding <noreply@damiengolding.com>
2019-10-16 22:37:38 +0900

    formatting and documentation

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+const RELATIVE_URLS = []
+
+/**
+ * @param {string} moduleString
+ * @return {*} TODO
+ */
 function require (moduleString) {
   forceCommonJSEnvironment()
   const url = resolveModuleUrl(moduleString)
+  const isRelativeModule = isRelativeURL(url)
+  let state
+  if(isRelativeModule){
+    state = {
+      url: moduleString
+    }
+    RELATIVE_URLS.push(state)
+  }
   const response = syncAJAX(url)
 
   appendCode(response)
+
+  if(isRelativeModule){
+    const ONE_ITEM = 1
+    RELATIVE_URLS.splice(RELATIVE_URLS.indexOf(state), ONE_ITEM)
+  }
+
   return exports
 }
 
+/**
+ * @param {string} url
+ * @return {string}
+ */
 function syncAJAX (url) {
   const xhr = new window.XMLHttpRequest()
   xhr.open('GET', url, false)
@@ -15,37 +39,63 @@ function syncAJAX (url) {
   return xhr.responseText
 }
 
+/**
+ * @param {string} code
+ */
 function appendCode (code) {
   const script = document.createElement('script')
   script.innerHTML = code
   window.document.head.appendChild(script)
+  //Assumes sync load so already loaded here.
 }
 
+/**
+ * @param {string} moduleString
+ * @return {string}
+ */
 function resolveModuleUrl (moduleString) {
   if (isURL(moduleString)) {
-    return moduleString
+    return moduleString//??This is not good enough. Nesting means requires should always be relative to the file being checked.Also there is the possibility of absolute urls and other(check require url resolution. library exists?)?.
   } else {
     // Assume is node_modules
     return getNodeModulesEntryPoint(moduleString)
   }
 }
 
+/**
+ * @param {string} moduleString
+ * @return {string}
+ */
 function getNodeModulesEntryPoint (moduleString) {
-  const moduleDir = `./node_modules/${moduleString}`
-  const packageJSON = syncAJAX(`${moduleDir}/package.json`)
+  const moduleDir = `./node_modules/${moduleString}`//??This SHOULD be relative to home. Should use home and include option for home.
+  const packageJSON = syncAJAX(`${moduleDir}/package.json`)//??If this fails should include warning info about using home option.
   const main = window.JSON.parse(packageJSON).main
   return `${moduleDir}/${main}`
 }
 
+/**
+ * @param {string} string
+ * @return {boolean}
+ */
 function isURL (string) {
   try {
-    string.substr(0, './'.length) === './' ||
-    string.substr(0, '../'.length) === '../' ||
+    isRelativeURL(string) ||
     new window.URL(string)
     return true
   } catch (_) {
     return false
   }
+}
+
+/**
+ * @param {string} string
+ * @return {boolean}
+ */
+function isRelativeURL (string) {
+  const FIRST_CHAR_INDEX = 0
+  const startsAtCurrentDirectory = string.substr(FIRST_CHAR_INDEX, './'.length) === './'
+  const startsAtPreviousDirectory = string.substr(FIRST_CHAR_INDEX, '../'.length) === '../'
+  return startsAtCurrentDirectory || startsAtPreviousDirectory
 }
 
 function forceCommonJSEnvironment () {
@@ -54,6 +104,29 @@ function forceCommonJSEnvironment () {
     window.module = {}
   }
   window.module.exports = window.exports
+}
+
+/**
+ * Should only be used when both relative/absolute paths.
+ * For example, not modules in format "module".
+ * @param {String} fileURL
+ * @param {String} inFileURL
+ * @return {String}
+ */
+function resolveInFileURL(fileURL = '', inFileURL = ''){//??
+  const fileDirectory = getURLDirectory(fileURL)
+  return urljoin(fileDirectory, inFileURL)//??
+}
+
+/**
+ * @param {string}
+ * @return {string}
+ */
+function getURLDirectory(fileURL){
+  const DELIMITER = '/'
+  const parts = fileURL.split(DELIMITER)
+  parts.pop()
+  return parts.join(DELIMITER)
 }
 
 // Comment out below to use as normal script
