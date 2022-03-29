bee99a89a2b77607f17863586d9ff806703d3496
Damien Golding <noreply@damiengolding.com>
2022-03-30 07:51:56 +0900

    pre-transfer commit

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Synchronous require using es6 imports.
 
+## Functionality
+
+- Allows for "require" while being synchronous. This matches how common js works in Node.js.
+- polyfilling module.exports and exports.
+- Uses es6 imports so can be used in non-built modern and no combining of import and require allowed environments.
+- Works on various URL formats: node_modules named, relative.
+
 ## Usage
 
 module:
@@ -20,4 +27,25 @@ script:
 
 ## Example
 
+```bash
+npx http-server ./
+# Go to http://localhost:8080
+```
+
 [Example](./index.html)
+
+## Other libraries, etc
+
+[Requirejs](https://requirejs.org/docs/api.html) is often used for implementing common js require functionality in browsers.
+However, it is "async" only: [Requirejs is async. Synchronous handling only works on already loaded modules.](https://stackoverflow.com/a/14201304/1764521)
+
+[Synchronous handling](https://stackoverflow.com/a/28010938/1764521) can be handled as shown in that link.
+This is similar to how it is implemented in this library.
+
+## Improvements
+
+This library has only been made to support what is needed.
+There are some features that should be added but has not been yet.
+If you find something that should be added, please create a detailed issue.
+
+Regards.
--- index.js
+++ index.js
@@ -1,37 +1,50 @@
+/**
+ * @typedef {{ url: string }} State
+ * @typedef {Record<String, any>} Exports
+ */
+
+const w = /** @type {Window & { module?: { exports?: Exports }, exports?: Exports }} */ (window)
+
+/**
+ * @type {State[]}
+ */
 const RELATIVE_URLS = []
 
 /**
  * @param {string} moduleString
- * @return {*} TODO
  */
-function require (moduleString) {
+function require(moduleString) {
   forceCommonJSEnvironment()
   const url = resolveModuleUrl(moduleString)
   const isRelativeModule = isRelativeURL(url)
-  let state
-  if(isRelativeModule){
-    state = {
-      url: moduleString
-    }
+  /**
+   * @type {State}
+   */
+  const state = {
+    url: ''
+  }
+  if (isRelativeModule) {
+    state.url = moduleString
     RELATIVE_URLS.push(state)
   }
   const response = syncAJAX(url)
 
   appendCode(response)
 
-  if(isRelativeModule){
+  if (isRelativeModule) {
     const ONE_ITEM = 1
     RELATIVE_URLS.splice(RELATIVE_URLS.indexOf(state), ONE_ITEM)
   }
 
-  return exports
+  // return exports
+  return w.exports
 }
 
 /**
  * @param {string} url
  * @return {string}
  */
-function syncAJAX (url) {
+function syncAJAX(url) {
   const xhr = new window.XMLHttpRequest()
   xhr.open('GET', url, false)
   xhr.send()
@@ -42,22 +55,23 @@ function syncAJAX (url) {
 /**
  * @param {string} code
  */
-function appendCode (code) {
+function appendCode(code) {
   const script = document.createElement('script')
   script.innerHTML = code
   window.document.head.appendChild(script)
-  //Assumes sync load so already loaded here.
+  // Assumes sync load so already loaded here.
 }
 
 /**
  * @param {string} moduleString
  * @return {string}
  */
-function resolveModuleUrl (moduleString) {
+function resolveModuleUrl(moduleString) {
   if (isURL(moduleString)) {
-    return moduleString//??This is not good enough. Nesting means requires should always be relative to the file being checked.Also there is the possibility of absolute urls and other(check require url resolution. library exists?)?.
+    return moduleString// TODO: This is not good enough. Nesting means requires should always be relative to the file being checked. Also there is the possibility of absolute urls and other(check require url resolution. library exists?)?.
   } else {
     // Assume is node_modules
+    // TODO: What about other resolving? local file? bower, etc.?
     return getNodeModulesEntryPoint(moduleString)
   }
 }
@@ -66,9 +80,9 @@ function resolveModuleUrl (moduleString) {
  * @param {string} moduleString
  * @return {string}
  */
-function getNodeModulesEntryPoint (moduleString) {
-  const moduleDir = `./node_modules/${moduleString}`//??This SHOULD be relative to home. Should use home and include option for home.
-  const packageJSON = syncAJAX(`${moduleDir}/package.json`)//??If this fails should include warning info about using home option.
+function getNodeModulesEntryPoint(moduleString) {
+  const moduleDir = `./node_modules/${moduleString}`// TODO: This SHOULD be relative to home. Should use home and include option for home.
+  const packageJSON = syncAJAX(`${moduleDir}/package.json`)// TODO: If this fails should include warning info about using home option.
   const main = window.JSON.parse(packageJSON).main
   return `${moduleDir}/${main}`
 }
@@ -77,10 +91,10 @@ function getNodeModulesEntryPoint (moduleString) {
  * @param {string} string
  * @return {boolean}
  */
-function isURL (string) {
+function isURL(string) {
   try {
     isRelativeURL(string) ||
-    new window.URL(string)
+      new window.URL(string)
     return true
   } catch (_) {
     return false
@@ -91,38 +105,40 @@ function isURL (string) {
  * @param {string} string
  * @return {boolean}
  */
-function isRelativeURL (string) {
+function isRelativeURL(string) {
   const FIRST_CHAR_INDEX = 0
   const startsAtCurrentDirectory = string.substr(FIRST_CHAR_INDEX, './'.length) === './'
   const startsAtPreviousDirectory = string.substr(FIRST_CHAR_INDEX, '../'.length) === '../'
   return startsAtCurrentDirectory || startsAtPreviousDirectory
 }
 
-function forceCommonJSEnvironment () {
-  window.exports = {} // Force reset.
-  if(typeof window.module !== 'object'){
-    window.module = {}
+function forceCommonJSEnvironment() {
+  w.exports = {} // Force reset.
+  if (typeof w.module !== 'object') {
+    w.module = {}
   }
-  window.module.exports = window.exports
+  w.module.exports = w.exports
 }
 
 /**
  * Should only be used when both relative/absolute paths.
  * For example, not modules in format "module".
- * @param {String} fileURL
- * @param {String} inFileURL
- * @return {String}
- */
-function resolveInFileURL(fileURL = '', inFileURL = ''){//??
-  const fileDirectory = getURLDirectory(fileURL)
-  return urljoin(fileDirectory, inFileURL)//??
-}
-
-/**
- * @param {string}
+ * @param {string} fileURL
+ * @param {string} inFileURL
  * @return {string}
  */
-function getURLDirectory(fileURL){
+/*
+function resolveInFileURL(fileURL = '', inFileURL = '') { // TODO: Not used.
+  const fileDirectory = getURLDirectory(fileURL)
+  return urljoin(fileDirectory, inFileURL) // TODO: urlJoin does not exist.
+}
+*/
+
+/**
+ * @param {string} fileURL
+ * @return {string}
+ */
+function getURLDirectory(fileURL) {
   const DELIMITER = '/'
   const parts = fileURL.split(DELIMITER)
   parts.pop()
